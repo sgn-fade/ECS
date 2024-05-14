14b252268fd954705db7c3456a6a797ee3521a75
fade <dimabreusgo@gmail.com>
2024-05-14 21:41:35 +0300

    added player

---
+++ Assets/Scripts/FollowComponent.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class FollowComponent
+{
+    public Transform Target;
+}
---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableComponent
+{
+    public float MoveSpeed = 0.0f;
+    public bool IsMoving = false;
+}
