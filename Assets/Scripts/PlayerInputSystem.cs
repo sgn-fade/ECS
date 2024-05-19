ff66f260da2bb8793f56e170307d45137a1dbfae
fade <dimabreusgo@gmail.com>
2024-05-19 10:20:29 +0300

    Player input system

--- Assets/Scripts/PlayerInputSystem.cs
+++ Assets/Scripts/PlayerInputSystem.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using Leopotam.EcsLite;
 using UnityEngine;
 
-public class PlayerInputSystem : IEcsRunSystem
+public class PlayerInputSystem : IEcsInitSystem, IEcsRunSystem
 {
+    EcsFilter _filter;
+    private EcsPool<InputComponent> _inputs;
     public void Run(IEcsSystems systems)
     {
-        float x = Inp
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if(x == 0 && y == 0) return;
+
+        foreach (int entity in _filter)
+        {
+            ref var inputComponent = ref _inputs.Get (entity);
+            inputComponent.Direction = new Vector2(x, y);
+        }
+    }
+
+    public void Init(IEcsSystems systems)
+    {
+        EcsWorld world = systems.GetWorld();
+        _filter = world.Filter<InputComponent>().End();
     }
 }
